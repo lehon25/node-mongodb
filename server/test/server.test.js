f5013aa77365c05af92c4a4105438de51cf74658
lehon25 <lehon_25@yahoo.com>
2018-08-23 20:15:20 +0700

    Add GET /todos route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -3,6 +3,7 @@ const request = require('supertest')
 
 const {app}= require('./../server')
 const {Todo}=require('./../models/todo')
+
   beforeEach((done)=>{
       Todo.remove({}).then(()=>done())
   })
